276d525624046b46dbce8d68ae3e4832c2bd8d5a
maxim.denisov <denisovmax1988@gmail.com>
2023-10-23 22:14:15 +0300

    Add utils

---
+++ src/utils.cpp
@@ -0,0 +1,8 @@
+/* ================ Макросы ================== */
+#ifdef DEBUG_MODE
+#define DEBUG(...) Serial.print(__VA_ARGS__)
+#define DEBUGLN(...) Serial.println(__VA_ARGS__)
+#else
+#define DEBUG(...)
+#define DEBUGLN(...)
+#endif
